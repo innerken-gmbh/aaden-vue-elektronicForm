de5dd244fe53b8efc887867c8231e8137192bcc2
AmyJiang0720 <1753810@tongji.edu.cn>
2021-03-18 10:09:40 +0100

    [Added]::完善表格添加错误窗口提示

--- elektronic-form/src/components/HelloWorld.vue
+++ elektronic-form/src/components/HelloWorld.vue
@@ -6,25 +6,25 @@
     </div>
     <div>
       <ul>Name:</ul>
-      <input v-model="name" placeholder="Bitte Name eingeben.">
+      <input v-model="formMess.name" placeholder="Bitte Name eingeben.">
     </div>
     <div>
       <ul>Vorname:</ul>
-      <input v-model="vorname" placeholder="Bitte Vorname eingeben.">
+      <input v-model="formMess.vorname" placeholder="Bitte Vorname eingeben.">
     </div>
     <div>
       <ul>Vollständige Anschrift:</ul>
-      <input v-model="anschrift" placeholder="Bitte Anschrift eingeben.">
+      <input v-model="formMess.anschrift" placeholder="Postleitzahl, Stadt">
     </div>
     <div>
       <ul>Telefonnummer:</ul>
-      <input v-model="tel" placeholder="Bitte Telefonnummer eingeben.">
+      <input v-model="formMess.tel" placeholder="Bitte Telefonnummer eingeben.">
     </div>
     <div>
       <ul>Zeitraum des Aufenthalts / ggf. Platz- oder Tischnummer:</ul>
-      <input v-model="zeit" placeholder="Bitte Zeitraum eingeben.">
+      <input v-model="formMess.zeit" placeholder="TT.MM.YYYY">
     </div>
-    <button style="margin: 50px 0px 0px">submit</button>
+    <button style="margin: 30px 0px 0px" @click="inspect">submit</button>
   </div>
 </template>
 
@@ -33,15 +33,74 @@ export default {
   name: 'HelloWorld',
   data(){
     return{
+      formMess:{
+        name:'',
+        vorname: '',
+        anschrift: '',
+        tel: '',
+        zeit: '',
+      },
+      checkdata:[
+        {
+          inspect: "",
+          msg: 'Name',
+        },
+        {
+          inspect: "",
+          msg: 'Vorname',
+        },
+        {
+          inspect: "",
+          msg: 'Anschrift',
+        },
+        {
+          inspect: "",
+          msg: 'Telefon',
+        },
+        {
+          inspect: "",
+          msg: 'Zeit',
+        }
+      ]
     }
   },
   props: {
     msg: String,
-    name: String,
-    vorname: String,
-    anschrift: String,
-    tel: String,
-    zeit: String
+  },
+  methods:{
+    have_empty(arr) {
+      for ( let key in arr) {
+        console.log(arr[key].inspect)
+        if(arr[key].inspect && arr[key].reg){
+          let reg = new RegExp(arr[key].reg)
+          const red_end = reg.test(arr[key].inspect);
+          if( !red_end ){
+            alert('Bitte korrekt ' + arr[key].msg + ' eingeben.')
+            return false
+          }
+        }else if(!arr[key].inspect){
+          alert('Bitte ' + arr[key].msg + ' eingeben.')
+          return false
+        }
+      }
+      alert('Vielen Dank für Ihren Unterstüzung!')
+      return true
+    },
+    inspect(){
+      this.assignData();
+      const end = this.have_empty(this.checkdata);
+      if(!end) return console.log('submit');
+    },
+    assignData(){
+      const arrForm = [];
+      for( let key in this.formMess){
+        arrForm.push(key);
+      }
+      for(let i = 0;i<this.checkdata.length;i++){
+        // const key = arrForm[i];
+        this.checkdata[i].inspect = this.formMess[arrForm[i]];
+      }
+    }
   }
 }
 </script>
@@ -65,5 +124,15 @@ li {
 }
 a {
   color: #42b983;
+}
+input{
+  border-width: 1px;
+  margin: auto;
+  max-width: 700px;
+  height: 30px;
+}
+button{
+  height: 30px;
+  font-size: 15px;
 }
 </style>
